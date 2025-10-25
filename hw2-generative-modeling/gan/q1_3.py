ff984bf3b39e3ba8c91fd1f1c6f4eb461f39d193
NikitaMalik2303 <nikitamalik2303@gmail.com>
2025-10-25 10:52:02 -0400

    hw2

---
+++ hw2-generative-modeling/gan/q1_3.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+from utils import get_args
+
+import torch
+
+from networks import Discriminator, Generator
+import torch.nn.functional as F
+from train import train_model
+
+
+def compute_discriminator_loss(
+    discrim_real, discrim_fake, discrim_interp, interp, lamb
+):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for discriminator.
+    # Do not use discrim_interp, interp, lamb. They are placeholders
+    # for Q1.5.
+    ##################################################################
+    l1 = F.binary_cross_entropy_with_logits(discrim_real, torch.ones_like(discrim_real))
+    l2 = F.binary_cross_entropy_with_logits(discrim_fake, torch.zeros_like(discrim_fake))
+    loss = l1 + l2
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+def compute_generator_loss(discrim_fake):
+    ##################################################################
+    # TODO 1.3: Implement GAN loss for the generator.
+    ##################################################################
+    loss = F.binary_cross_entropy_with_logits(discrim_fake, torch.ones_like(discrim_fake))
+    ##################################################################
+    #                          END OF YOUR CODE                      #
+    ##################################################################
+    return loss
+
+
+if __name__ == "__main__":
+    args = get_args()
+    gen = Generator().cuda()
+    disc = Discriminator().cuda()
+    prefix = "data_gan/"
+    os.makedirs(prefix, exist_ok=True)
+
+    train_model(
+        gen,
+        disc,
+        num_iterations=int(3e4),
+        batch_size=256,
+        prefix=prefix,
+        gen_loss_fn=compute_generator_loss,
+        disc_loss_fn=compute_discriminator_loss,
+        log_period=1000,
+        amp_enabled=not args.disable_amp,
+    )
